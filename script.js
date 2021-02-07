4377c64c748ba00cd53d65dc82a7d35aee7dc078
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-02-06 19:09:05 -0500

    add infinite scroll

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 const count = 10;
 const apiKey = '4BS4l4xYkQ-qzExvs38Ksy45G6iPi-oALhPah3G25wA';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
 
 function setAttributes(element, attributes) {
 	for (const key in attributes) {
@@ -14,23 +25,29 @@ function setAttributes(element, attributes) {
 }
 
 function displayPhotos() {
-	photosArray.forEach((photo) => {
-		const item = document.createElement('a');
-		
-		setAttributes(item, {
-			href: photo.link.html,
-			target: '_blank',
-		});
-		const img = document.createElement('img');
-		
-		setAttributes(item, {
-			src: photo.urls.regular,
-			alt: photo.alt_description,
-			title: photo.alt_description,
-		});
-		item.appendChild(img);
-		imageContainer.appendChild(item);
-	});
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to full photo
+    const item = document.createElement('a');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+    // Create <img> for photo
+    const img = document.createElement('img');
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // Event Listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded);
+    // Put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
 }
 
 async function getPhotos() {
@@ -42,5 +59,12 @@ async function getPhotos() {
 		// catch errror here
 	}
 }
+
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 getPhotos();
