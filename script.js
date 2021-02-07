99f4bd243cb6509a43ed4cc9e0f5326ab06982fb
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2021-02-06 20:26:54 -0500

    improve pic loading performance

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 30;
-
+let imageCount = 5;
 const apiKey = 'API KEY';
-const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
 // Check if all images were loaded
 function imageLoaded() {
@@ -18,6 +17,8 @@ function imageLoaded() {
   if (imagesLoaded === totalImages) {
     ready = true;
     loader.hidden = true;
+    imageCount = 30;
+    let apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
   }
 }
 
